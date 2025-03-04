7db38d2fbc4872c8c3a562d21d9b2ebbacf79667
faysal725 <afaysal725@gmail.com>
2025-03-04 23:47:09 +0600

    login functionality added

--- actions/auth_action.js
+++ actions/auth_action.js
@@ -1,8 +1,8 @@
 "use server";
 
 import { createAuthSession } from "@/lib/auth";
-import { hashUserPassword } from "@/lib/hash";
-import { createUser } from "@/user";
+import { hashUserPassword, verifyPassword } from "@/lib/hash";
+import { createUser, getUserByEmail } from "@/user";
 import { redirect } from "next/navigation";
 
 export async function signup(prevState, formData) {
@@ -42,4 +42,39 @@ export async function signup(prevState, formData) {
     }
     throw error;
   }
+}
+
+export async function login(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  let existingUser = getUserByEmail(email);
+
+  if (!existingUser) {
+    return {
+      errors: {
+        email: "could not authenticate user, please check credentials",
+      },
+    };
+  }
+  const isValidPassword = verifyPassword(existingUser.password, password);
+
+  if (!isValidPassword) {
+    return {
+        errors: {
+          password: "could not authenticate user, please check credentials",
+        },
+      };
+  }
+
+  await createAuthSession(existingUser.id)
+  redirect('/training')
+}
+
+
+export async function auth(mode, prevState, formData) {
+    if (mode === 'login') {
+        return login(prevState, formData)
+    }
+    return signup(prevState, formData)
 }
