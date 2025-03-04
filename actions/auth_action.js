76b9435dd2babddb47058db08cbe7142ba478e16
faysal725 <afaysal725@gmail.com>
2025-03-04 15:08:34 +0600

    first commit

---
+++ actions/auth_action.js
@@ -0,0 +1,45 @@
+"use server";
+
+import { hashUserPassword } from "@/lib/hash";
+import { createUser } from "@/user";
+import { redirect } from "next/navigation";
+
+export async function signup(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  // validate the data
+  let errors = {};
+
+  if (!email.includes("@")) {
+    errors.email = "Please enter a valid email address.";
+  }
+  if (password.trim().length < 8) {
+    errors.password = "Password must be at least 8 char long";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return {
+      errors: errors,
+    };
+  }
+
+  //   password hashing is done
+  const hashedPassword = hashUserPassword(password);
+  try {
+    // store it in the db
+    createUser(email, hashedPassword);
+  } catch (error) {
+    if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      return {
+        errors: {
+          email: "email already exist.",
+        },
+      };
+    }
+    throw error;
+  }
+  
+
+  redirect('/training')
+}
